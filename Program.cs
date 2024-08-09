699c14bb1ef66d67a215ae65cbff83a0207c3709
62127702024.2 <62127702024.2@62DEFS0J504>
2024-08-09 20:56:36 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double numero;
+int numero2;
+do
+{
+    Console.WriteLine("Digite um número inteiro: ");
+   numero = Convert.ToDouble(Console.ReadLine());
+    numero2 = (int)Convert.ToDouble(numero);
+}  while (numero != numero2);
+Console.WriteLine($"^Você digitou um numero inteiro: {numero}");
